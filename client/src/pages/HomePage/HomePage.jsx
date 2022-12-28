7c0a30e179daff0a275eaa0ae87f67094008fdee
farnaz <farnaz.towhidi@gmail.com>
2022-12-28 15:31:46 -0500

    index updates

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -3,70 +3,122 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import Collapse from 'react-bootstrap/Collapse';
-import { Link } from "react-router-dom";
+import Carousel from 'react-bootstrap/Carousel';
+// import header1 from "../../images/background2.jpg";
+// import header2 from "../../images/FarnazTowhdi_header2.jpg";
+// import header3 from "../../images/FarnazTowhdi_header3.jpg";
+
 
 export default function HomePage() {
   const [open, setOpen] = useState(false); 
 
 
   return (
-    <Container>
-      <Row><Col><img src="../images/farnaztowhidi.jpg" /></Col></Row>
-      <Row>
-        <Col className='font_Tajawal White'>FARNAZ TOWHIDI</Col>
-      </Row>
-      <Row>
-        <Col className='font_Tajawal Orange size_30'>FRONTEND DEVELOPER</Col>
-      </Row>
-      <Row className='height_30'>
-        <Col className='font_Tajawal White size_30'>EXPERIENCES</Col>
-      </Row>
-      <Row>
-        <Col lg={1} className="Orange">IBM</Col>     
-        <Col lg={7}> 
-              <h3 className="Orange">@ Quality Assurance, 2020</h3>
-              <ul>
-                <li>Part of security intelligence team of 10 working on QRadar admin</li>
-                <li>Knowledge on QRadar log and network activity and reports</li>
-                <li> Created and edited automated test cases, manual, automation and regression testing</li>
-                <li>Technologies used: Selenium, QRadar, Eclipse, Test Rail, Git, Git Hub</li>
-              </ul>           
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={1} className="Orange">Canadian Institute of Cybersecurity</Col>     
-        <Col lg={8}> 
-              <h3 className="Orange">@ Frontend Developer (contract), 2019</h3>
-              <ul>
-                <li>Designed and built website for International Conference on Privacy, Security and Trust, including information on events, workshops, organizers, …</li>
-                <li>Technologies used: Html, CSS, ASP classic</li>              
-              </ul>           
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={1} className="Orange">Biossentials</Col>     
-        <Col lg={8}> 
-              <h3 className="Orange">@ Frontend Developer (contract), 2019</h3>
-              <ul>
-                <li>Worked closely with a designer to create new comprehensive layout</li>
-                <li>Coded company website from ground up based on new graphic design, along with SEO audits that considered on page and off-page</li>
-                <li>Technologies used: HTML, CSS, ASP Classic, SEO</li>
-              </ul>           
-        </Col>
-      </Row>
+    <Container fluid>
+      {/* <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={header1}
+          alt="First slide"
+        />       
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={header2}
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={header3}
+          alt="Third slide"
+        />
+
+      </Carousel.Item>
+      </Carousel> */}
       
-      <Button
-        onClick={() => setOpen(!open)}
-        aria-controls="example-collapse-text"
-        aria-expanded={open}
-      >
-        click
-      </Button>
+     
+      <section id="overview">
+        <Row>
+          <Col className='font_Tajawal'>FARNAZ TOWHIDI</Col>
+        </Row>
+        <Row lg={12} style={{display:"flex", }}>
+          <Col lg={5} style={{writingMode: "vertical-rl"}}><span>FRONTEND DEVELOPER</span></Col>    
+          <Col lg={2} style={{textAlign:"center"}} >
+            <img src="../../images/FarnazTowhidi.jpg" />
+          </Col> 
+          <Col lg={5}><span style={{writingMode:"vertical-rl", }}>FRONTEND DEVELOPER</span></Col>            
+        </Row>
+        <Row>
+          <Col className='font_Tajawal'>FARNAZ TOWHIDI</Col>
+        </Row>
+      </section>
+
+      <section id="experience">
+        <Row>
+          <Col className='idx_sections_header' >EXPERIENCES</Col>
+        </Row>
+        
+        <Row lg={12} className="justify-content-md-center">
+          <Col lg={4} className="idx_sections_subheader" >IBM <br />FREDERICTON</Col>     
+          <Col lg={8}> 
+                <ul>
+                  <li>@Quality Assurance, 2020</li>
+                  <li>Part of security intelligence team of 10 working on QRadar admin</li>
+                  <li>Knowledge on QRadar log and network activity and reports</li>
+                  <li> Created and edited automated test cases, manual, automation and regression testing</li>
+                  <li>Technologies used: Selenium, QRadar, Eclipse, Test Rail, Git, Git Hub</li>
+                </ul>           
+          </Col>
+        </Row>
+        <Row lg={12} className="justify-content-md-center">
+          <Col lg={4} className="idx_sections_subheader">CIC <br />CANADA</Col>     
+          <Col lg={8}> 
+                <ul>
+                  <li>@Frontend Developer (contract), 2019</li>
+                  <li>Designed and built website for International Conference on Privacy, Security and Trust, including information on events, workshops, organizers, …</li>
+                  <li>Technologies used: Html, CSS, ASP classic</li>              
+                </ul>           
+          </Col>
+        </Row>
+        <Row lg={12} className="justify-content-md-center">
+          <Col lg={4} className="idx_sections_subheader">BIOSSENTIALS <br /> MALAYSIA</Col>     
+          <Col lg={8}> 
+                <ul>
+                  <li>@Frontend Developer (contract), 2019</li>
+                  <li>Worked closely with a designer to create new comprehensive layout</li>
+                  <li>Coded company website from ground up based on new graphic design, along with SEO audits that considered on and off-page</li>
+                  <li>Technologies used: HTML, CSS, ASP Classic, SEO</li>
+                </ul>           
+          </Col>
+        </Row>
+      </section>
       
-      <Row>
-        <Col className='font_Tajawal White size_30'>EDUCATION</Col>
-      </Row>
+      <section id="education">
+        <Row>
+          <Col className='idx_sections_header'>EDUCATION</Col>
+        </Row>
+        <Row lg={12} className="justify-content-md-center">
+          <Col lg={4} className="idx_sections_subheader"></Col>     
+          <Col lg={8}> 
+                <ul>
+                  <li>GENERAL ASSEMBLY, Software Engineering Immersive</li>
+                  <li>PHD of Computer Science, UTM University, Kuala Lumpur, Malaysia</li>  
+                  <li>Master of Information Security, UTM University, Kuala Lumpur, Malaysia	</li>              
+                </ul>           
+          </Col>
+        </Row>
+      </section>
+      
+     
+      
+      
+      <section>
+
+      </section>
     </Container>
   )
 }
