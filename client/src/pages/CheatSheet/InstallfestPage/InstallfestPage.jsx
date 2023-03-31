7e70a5baac4d7cb52075de8270fe53d201a10c71
farnaz <farnaz.towhidi@gmail.com>
2023-03-31 08:20:56 -0400

    update installfest

--- client/src/pages/CheatSheet/InstallfestPage/InstallfestPage.jsx
+++ client/src/pages/CheatSheet/InstallfestPage/InstallfestPage.jsx
@@ -36,15 +36,14 @@ export default function InstallfestPage() {
           <span className="fw-bold">Node package manager:</span> npm install request <br />
           <span className="fw-bold">nodemon:</span> npm install -g nodemon
 
-          <h5>Create Express App</h5>
-          express -e projectName  (e for ejs files)
-          <h5>Install node modules</h5>
-          npm i
+          <h5 style={{color:"red"}}>Create Express App</h5>
+          express -e backend  (e for ejs files) <br />
+          <span className="fw-bold">Install node modules</span>: npm i
           
         
           <h5>EXPRESS GENERATOR</h5>
-          Replace installation for "npm i" and "npm install request": <br />
           npm install -g express-generator <br />
+          <span className="fw-bold">Install Express App</span>: express -e Backend <br/>
           express -e express-todos (<span className="fw-bold">NOTE:</span> Rename app.js to server.js and browse to bin, www, Rename require app to server )<br />
           <span className="fw-bold">Create node modules:</span> npm i <br />
 
@@ -64,7 +63,8 @@ export default function InstallfestPage() {
           pip3 install bot3 <br />
 
           <h5>REACT</h5>
-          npx create-react-app app-name <br />
+          <span className="fw-bold">Install React APP</span>: npx create-react-app frontend <br />
+          npm i express morgan serve-favicon <br />
           npx clear-npx-cashe <br />
           npm install react-syntax-highlighter --save <br />
           npm i react-code-blocks<br />
@@ -74,6 +74,8 @@ export default function InstallfestPage() {
           <span className="fw-bold">Install Dray and Drop</span>: npm install react-beautiful-dnd --save<br />
           <span className="fw-bold">Install React Sidebar</span>: npm install react-pro-sidebar<br />
           <span className="fw-bold">Install React Icon</span>: npm install react-icons<br />
+          <span className="fw-bold">Install React aws-skd</span>: npm install aws-sdk<br />
+          
 
 
           npm install axios <br />
@@ -83,13 +85,18 @@ export default function InstallfestPage() {
 
           
 
-          <h5>MERN STACK</h5>
-            1- Generate the React app: npx create-react-app mern-infrastructure <br />
-            2- Build the React app's production code: npm run build <br />
-            3- Code the skeleton Express app: npm i express morgan serve-favicon<br/>
-            4- Create and Code the Express App (server.js): touch server.js
-            5- Define the "catch all" route in the Express backend< br />
-            6- Test the Express server: nodemon server
+          <h5>MERN STACK David Method</h5>
+            <span className="fw-bold"> 1- Generate the React app: </span>npx create-react-app mern-infrastructure <br />
+            <span className="fw-bold"> 2- Build the React app's production code: </span>npm run build <br />
+            <span className="fw-bold"> 3- Code the skeleton Express app: </span>npm i express morgan serve-favicon<br/>
+            <span className="fw-bold"> 4- Create and Code the Express App (server.js): </span>touch server.js
+            <span className="fw-bold"> 5- Define the "catch all" route in the Express backend</span>< br />
+            <span className="fw-bold"> 6- Test the Express server: </span>nodemon server
+
+            <h5>MERN STACK Martin Method</h5>
+            <span className="fw-bold">Create React APP:</span> npx create-react-app frontend <br />
+            <span className="fw-bold">Create Express App:</span>express -e backend <br />
+            <span className="fw-bold">Install node modules</span>: npm i <br />
           </Col>
         </Row>
         
